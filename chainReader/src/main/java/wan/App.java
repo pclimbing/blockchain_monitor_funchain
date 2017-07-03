da3912c904f3039f2e826b919cc54f8c920ba6b2
pandong <19135978@qq.com>
2017-07-03 14:52:39 +0800

    init project

---
+++ chainReader/src/main/java/wan/App.java
@@ -0,0 +1,209 @@
+package wan;
+
+import cn.hyperchain.sdk.crypto.ECPriv;
+import cn.hyperchain.sdk.rpc.HyperchainAPI;
+import cn.hyperchain.sdk.rpc.Transaction.Transaction;
+import cn.hyperchain.sdk.rpc.function.FuncParamReal;
+import cn.hyperchain.sdk.rpc.function.FunctionDecode;
+import cn.hyperchain.sdk.rpc.function.FunctionEncode;
+import cn.hyperchain.sdk.rpc.returns.*;
+import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * goo
+ *
+ */
+
+public class App
+{
+
+    final static String SOLC_CODE = "contract Accumulator{ uint32 sum = 0; function increment(){ sum = sum + 1;\n" +
+            "} function getSum() returns(uint32){ return sum; } function add(uint32 num1,uint32 num2) { sum = sum+num1+num2; } }";
+
+    public static void main( String[] args )
+    {
+
+        try {
+            HyperchainAPI hyperchain = new HyperchainAPI();
+            InfluxWriter influxdb = new InfluxWriter();
+
+
+//            ECPriv ecKey = createAccount(hyperchain);
+//            CompileReturn complieResult = compile(hyperchain, SOLC_CODE);
+//            String contractAddress = deploy(hyperchain, complieResult.getBin().get(0), ecKey);
+////            invokeNoParamFunc(hyperchain, contractAddress, complieResult.getAbi().get(0), ecKey, "increment");
+//            invokeNoParamFunc(hyperchain, contractAddress, complieResult.getAbi().get(0), ecKey, "getSum");
+//            query(hyperchain);
+
+
+
+            StdReturn re = hyperchain.getTx( "1", "");
+            System.out.println(re.getResult());
+
+            insertTrans(hyperchain, influxdb);
+            insertBlocks(hyperchain, influxdb);
+//            insertBlocktoInflux(hyperchain, influxdb);  //abandoned
+            insertNodestoInflux(hyperchain, influxdb);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void insertTrans(HyperchainAPI hyperchain, InfluxWriter influxdb) {
+
+        StdReturn re = hyperchain.getTx( "1", "");
+//        System.out.println(re.getResult());
+        JSONArray transArr = JSONArray.fromObject(re.getResult());
+
+        Iterator<Object> it = transArr.iterator();
+        while (it.hasNext()) {
+            JSONObject ob = (JSONObject) it.next();
+            System.out.println(ob);
+            influxdb.write_trans(ob.toString());
+
+        }
+    }
+
+    public static void insertBlocks(HyperchainAPI hyperchain, InfluxWriter influxdb){
+
+        //get newest block
+        BlockReturn blockReturn = hyperchain.getLatestBlock();
+        JSONObject bkjson = JSONObject.fromObject(blockReturn.getResult());
+        String number = bkjson.getString("number");
+        int numberInt = Integer.parseInt(number.replaceAll("^0[x|X]", ""), 16);
+
+
+        //get and insert all blocks
+        ArrayList<BlockReturn> blockReturns = hyperchain.getBlocks( new BigInteger( "1"),
+                new BigInteger( "" + numberInt));
+
+        JSONArray blockArr = JSONArray.fromObject(blockReturns.get(0).getResult());
+
+        Iterator<Object> it = blockArr.iterator();
+        while (it.hasNext()) {
+            JSONObject ob = (JSONObject) it.next();
+            //System.out.println(ob);
+            influxdb.write_block(ob.toString());
+
+        }
+
+        //insert avg block time
+        SingleValueReturn singleValueReturn = hyperchain.getAvgGenTimeByBlkNum( new BigInteger( "1"),
+                new BigInteger( "" + numberInt));
+        int arvTimeInt = Integer.parseInt(singleValueReturn.getResult().replaceAll("^0[x|X]", ""), 16);
+        System.out.println("平均出块时间： " + arvTimeInt);
+        influxdb.write_avgTime( arvTimeInt + "");
+    }
+
+    public static void insertBlocktoInflux(HyperchainAPI hyperchain, InfluxWriter influxdb) {
+        BlockReturn blockReturn = hyperchain.getLatestBlock();
+        influxdb.write_block(blockReturn.getResult());
+
+    }
+
+    //insert all node into influxdb
+    //will flush nodes first to ensore all nodes remain only one in db
+    public static void insertNodestoInflux(HyperchainAPI hyperchain, InfluxWriter influxdb){
+//        influxdb.removeNodes(); //move to influxWriter
+        ArrayList<NodeInfoReturn> nodeInfoReturns= hyperchain.getNodes();
+        influxdb.write_nodes(nodeInfoReturns);
+    }
+
+
+
+
+
+    // test create account, compile code, deploy code, and invoke code
+    public static void test(HyperchainAPI hyperchain){
+//        ECPriv ecKey = createAccount(hyperchain);
+//        CompileReturn complieResult = compile(hyperchain, SOLC_CODE);
+//        System.out.println(complieResult.getAbi());
+//        String contractAddress = deploy(hyperchain, complieResult.getBin().get(0), ecKey);
+//        invokeNoParamFunc(hyperchain, contractAddress, complieResult.getAbi().get(0), ecKey, "increment");
+//        invokeNoParamFunc(hyperchain, contractAddress, complieResult.getAbi().get(0), ecKey, "getSum");
+//        query(hyperchain);
+
+    }
+
+    public static void query(HyperchainAPI hyperchain) {
+        SingleValueReturn r = hyperchain.getTx("1", "");
+        System.out.println(r.getResult());
+    }
+
+    public static CompileReturn compile(HyperchainAPI hyperchain, String solcCode) {
+        CompileReturn complieResult = hyperchain.compileContract(solcCode);
+        return complieResult;
+    }
+
+    public static String deploy(HyperchainAPI hyperchain, String bin, ECPriv ecKey) throws Exception {
+        Transaction transaction = new Transaction(ecKey.address(),
+                bin,
+                false);
+        transaction.sign(ecKey);
+
+        SingleValueReturn result = hyperchain.deployContract(transaction);
+        ReceiptReturn deployReceipt = hyperchain.getTransactionReceipt(result.getResult());
+
+        return deployReceipt.getContractAddress();
+    }
+
+    public static void invokeNoParamFunc(HyperchainAPI hyperchain, String contractAddress, String abi, ECPriv ecKey, String func) throws Exception {
+        String input = FunctionEncode.encodeFunction(func);
+
+        Transaction transaction = new Transaction(ecKey.address(),
+                contractAddress,
+                input,
+                false);
+        transaction.sign(ecKey);
+
+        SingleValueReturn result = hyperchain.invokeContract(transaction);
+
+        ReceiptReturn solcReceipt = hyperchain.getTransactionReceipt(result.getResult());
+        System.out.println(func + "  " + solcReceipt.getResult());
+        System.out.println(FunctionDecode.resultDecode(func,abi,solcReceipt.getRet()));
+    }
+
+    public static void sendTx(HyperchainAPI hyperchain, ECPriv ecKey) throws Exception {
+        Transaction transaction = new Transaction(ecKey.address(),
+                "000f1a7a08ccc48e5d30f80850cf1cf283aa3abd",
+                100,
+                false);
+        transaction.sign(ecKey);
+
+        StdReturn result = hyperchain.sendTx(transaction);
+        System.out.println(result.getResult());
+    }
+
+    public static ECPriv createAccount(HyperchainAPI hyperchain) throws Exception {
+        String accountJson = hyperchain.newAccount( "123");
+        System.out.println(accountJson);
+
+        ECPriv ecKey = hyperchain.decryptAccount(accountJson, "123"); //获取地址
+
+        return ecKey;
+    }
+
+    public static void invoke(HyperchainAPI hyperchain, String contractAddress, ECPriv ecKey) throws Exception {
+        FuncParamReal param1 = new FuncParamReal("uint32", 1);
+        FuncParamReal param2 = new FuncParamReal("uint32", 2);
+        String input = FunctionEncode.encodeFunction("add", param1, param2);
+
+        Transaction transaction = new Transaction(ecKey.address(),
+                contractAddress,
+                input,
+                false);
+        transaction.sign(ecKey);
+
+        SingleValueReturn result = hyperchain.invokeContract(transaction);
+        ReceiptReturn solcReceipt = hyperchain.getTransactionReceipt(result.getResult());
+    }
+}
---
+++ chainReader/src/main/java/wan/InfluxWriter.java
@@ -0,0 +1,244 @@
+package wan;
+
+import cn.hyperchain.sdk.rpc.returns.NodeInfoReturn;
+import net.sf.json.JSONObject;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.sql.Time;
+
+/**
+ * Created by dong on 2017/6/26.
+ */
+
+
+public class InfluxWriter {
+
+    InfluxDB influxDB;
+    String dbName;
+
+    public InfluxWriter() throws Exception {
+        try {
+            InputStream e = this.getClass().getClassLoader().getResourceAsStream("influx.properties");
+//            File file = new File("/Users/md760/wanda/projects/influx.properties");
+//            InputStream e =  new FileInputStream(file);
+
+            Properties properties = new Properties();
+            properties.load(e);
+
+//
+//            String node = properties.getProperty("node");
+//            JSONObject nodes = JSONObject.fromObject(node);
+//            String nodeUrl = nodes.getString("nodes");
+//            influxDB.deleteDatabase(dbName);
+
+            String url = properties.getProperty("influx_url");
+            String u = properties.getProperty("influx_username");
+            String p = properties.getProperty("influx_password");
+            String db = properties.getProperty("dbname");
+
+            influxDB = InfluxDBFactory.connect(url );
+            dbName = db;
+            influxDB.deleteDatabase(dbName);
+            influxDB.createDatabase(dbName);
+
+        } catch (Exception var18) {
+            var18.printStackTrace();
+        }
+
+    }
+
+
+    public void write_nodes(ArrayList<NodeInfoReturn> nodeInfoReturns){
+        this.deleteMeasurement("nodes");
+        for (NodeInfoReturn nodeInfoReturn : nodeInfoReturns){
+            System.out.println(nodeInfoReturn.getIp());
+//            System.out.println(nodeInfoReturn.getId());
+//            System.out.println(nodeInfoReturn.getPrimary());
+//            System.out.println(nodeInfoReturn.getDelay());
+            this.write_node(nodeInfoReturn.getIp(), nodeInfoReturn.getId(), nodeInfoReturn.getPrimary(), nodeInfoReturn.getDelay());
+        }
+    }
+    public void write_node(String ip, int id, boolean primary, BigInteger delay){
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .retentionPolicy("autogen")
+                .consistency(InfluxDB.ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("nodes")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .tag("node", ip)
+                .addField("ip", ip)
+                .addField("id", id)
+                .addField("primary", primary)
+                .addField("delay", delay)
+                .build();
+        batchPoints.point(point1);
+//        this.deleteMeasurement()
+        influxDB.write(batchPoints);
+    }
+
+    public void write_blocks(String bk) {
+        JSONObject bkjson = JSONObject.fromObject(bk);
+        String number = bkjson.getString("number");
+
+    }
+
+    public void write_block(String bk){
+
+        JSONObject bkjson = JSONObject.fromObject(bk);
+        String version = bkjson.getString("version");
+        String number = bkjson.getString("number");
+        String hash = bkjson.getString("hash");
+        String parentHash = bkjson.getString("parentHash");
+        Long time = bkjson.getLong("writeTime");
+        String avgTime = bkjson.getString("avgTime");
+        String txcounts = bkjson.getString("txcounts");
+        String merkleRoot = bkjson.getString("merkleRoot");
+
+        int txcountsInt = Integer.parseInt(txcounts.replaceAll("^0[x|X]", ""), 16);
+        int numberInt = Integer.parseInt(number.replaceAll("^0[x|X]", ""), 16);
+
+
+//        System.out.println(System.currentTimeMillis());
+//        System.out.println(time);
+//        System.out.println( System.currentTimeMillis());
+
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .retentionPolicy("autogen")
+                .consistency(InfluxDB.ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("blocks")
+                .time(time/1000000, TimeUnit.MILLISECONDS)
+                .tag("block", number)
+                .addField("version", version)
+                .addField("number", numberInt)
+                .addField("hash", hash)
+                .addField("parentHash", parentHash)
+                .addField("avgTime", avgTime)
+                .addField("txcounts", txcountsInt)
+                .addField("merkleRoot", merkleRoot)
+                .build();
+        batchPoints.point(point1);
+
+        this.deleteByField("blocks", "block", number);
+        influxDB.write(batchPoints);
+    }
+
+    public void write_avgTime(String t){
+        //delete the former one
+
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .retentionPolicy("autogen")
+                .consistency(InfluxDB.ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("avgBlockTime")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .addField("avgblocktime", t)
+                .build();
+        batchPoints.point(point1);
+
+        this.deleteMeasurement("avgBlockTime");
+        influxDB.write(batchPoints);
+    }
+
+
+
+    public void write_trans(String transStr){
+        JSONObject trjson = JSONObject.fromObject(transStr);
+
+        String version = trjson.getString("version");
+        String hash = trjson.getString("hash");
+        String blockNumber = trjson.getString("blockNumber");      // 0x
+        String blockHash =  trjson.getString("blockHash");
+        String txIndex =  trjson.getString("txIndex");           //0x
+        String from = trjson.getString("from");
+        String to = trjson.getString("to");
+        String amount = trjson.getString("amount");       //0x
+        Long   timestamp = trjson.getLong("timestamp");
+        Long   nonce = trjson.getLong("nonce");
+        String executeTime = trjson.getString("executeTime");     //0x
+        String payload = trjson.getString("payload");
+
+        int blockInt = Integer.parseInt(blockNumber.replaceAll("^0[x|X]", ""), 16);
+        int txIndexInt = Integer.parseInt(txIndex.replaceAll("^0[x|X]", ""), 16);
+        int amountInt = Integer.parseInt(amount.replaceAll("^0[x|X]", ""), 16);
+        int executeTimeInt = Integer.parseInt(executeTime.replaceAll("^0[x|X]", ""), 16);
+
+        BatchPoints batchPoints = BatchPoints
+                .database(dbName)
+                .retentionPolicy("autogen")
+                .consistency(InfluxDB.ConsistencyLevel.ALL)
+                .build();
+        Point point1 = Point.measurement("transactions")
+                .time(timestamp, TimeUnit.MILLISECONDS)
+                .tag("tranHash", hash)
+                .addField("version", version)
+                .addField("blockNumber", blockInt)
+                .addField("hash", hash)
+                .addField("blockHash", blockHash)
+                .addField("txIndex", txIndexInt)
+                .addField("from", from)
+                .addField("to", to)
+                .addField("amount", amountInt)
+                .addField("nonce", nonce)
+                .addField("executeTime", executeTimeInt)
+                .addField("payload", payload)
+                .build();
+        batchPoints.point(point1);
+
+        this.deleteByField("transactions", "tranHash", hash);
+        influxDB.write(batchPoints);
+
+    }
+
+
+    public void deleteMeasurement(String measurement){
+
+        String command = "DELETE FROM " + measurement;
+
+        final Query query = new Query(command, dbName, true);
+
+        try {
+            QueryResult.Result result = influxDB.query(query).getResults().get(0);
+
+            if (result.hasError()) {
+                throw new RuntimeException(result.getError());
+            }
+        } catch (RuntimeException e) {
+            throw e;
+        }
+    }
+
+    public void deleteByField(String measurement, String tag, String value){
+
+        String command = "DELETE FROM " + measurement + " WHERE " +  tag + " = " + value ;
+
+        final Query query = new Query(command, dbName, true);
+
+        try {
+            QueryResult.Result result = influxDB.query(query).getResults().get(0);
+
+            if (result.hasError()) {
+                throw new RuntimeException(result.getError());
+            }
+        } catch (RuntimeException e) {
+            throw e;
+        }
+    }
+}
